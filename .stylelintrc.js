d124b8454b8d8b75e92f216799d2b48c6e924792
Yuehu Lin <dev@fntsr.tw>
2023-01-01 18:03:37 +0800

    add stylelint dev support

---
+++ .stylelintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // https://stylelint.io/user-guide/rules
+  "extends": [
+    "stylelint-config-standard",
+    "stylelint-config-prettier"
+  ],
+  "rules": {
+    "indentation": "2"
+  }
+}
