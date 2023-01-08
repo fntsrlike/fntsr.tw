0aeae8ee655413a6e347c1b251310485d491a663
Yuehu Lin <dev@fntsr.tw>
2023-01-09 05:38:54 +0800

    feature(content) add routes page direct to content

---
+++ pages/[...slug].vue
@@ -0,0 +1,10 @@
+<template>
+  <main>
+    <ContentDoc>
+      <template #not-found>
+        <p>404 not found</p>
+        <p>back to <nuxt-link to="/">index</nuxt-link>.</p>
+      </template>
+    </ContentDoc>
+  </main>
+</template>
