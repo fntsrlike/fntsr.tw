5ccbb28e3ec211c34e7438091e7b434e98ba89cc
Yuehu Lin <dev@fntsr.tw>
2023-01-09 06:03:46 +0800

    add typography

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,6 +1,6 @@
 <template>
-  <main>
-    <ContentDoc>
+  <main class="mx-auto max-w-2xl px-6 py-6">
+    <ContentDoc class="prose">
       <template #not-found>
         <p>404 not found</p>
         <p>back to <nuxt-link to="/">index</nuxt-link>.</p>
