2d1fe8b7e29158f495c3b935254ae1832a297bfc
Yuehu Lin <dev@fntsr.tw>
2023-01-11 02:14:21 +0800

    improve mermaid to ssr

---
+++ components/content/mermaid-graph-build-in.vue
@@ -0,0 +1,35 @@
+<!-- eslint-disable prettier/prettier -->
+<template>
+  <pre class="mermaid">
+    graph TD
+    A[Client] --> B[Load Balancer]
+        B --> C[Server01]
+        B --> D[Server02]
+  </pre>
+</template>
+
+<script setup>
+import mermaid from 'mermaid'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  mermaid.initialize({
+    startOnLoad: true,
+  })
+  mermaid.init()
+})
+</script>
+
+<style>
+pre.mermaid {
+  background-color: rgba(0, 0, 0, 0);
+  padding: 0;
+  margin: 0;
+  border-radius: 0;
+  box-sizing: border-box;
+  border-width: 0;
+  border-style: solid;
+  border-color: #e5e7eb;
+  color: var(--tw-prose-body);
+}
+</style>
---
+++ components/content/mermaid-graph.vue
@@ -0,0 +1,19 @@
+<!-- eslint-disable prettier/prettier -->
+<template>
+  <h4>Outside in Slot</h4>
+  <div>
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import mermaid from 'mermaid'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  mermaid.initialize({
+    startOnLoad: true,
+  })
+  mermaid.init()
+})
+</script>
