311c185ec5db0e75fde48dcdaf5a8c1e8845793b
Yuehu Lin <dev@fntsr.tw>
2023-03-27 06:24:18 +0800

    feature(style): add default layout (to split)

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,10 +1,8 @@
 <template>
-  <main class="mx-auto max-w-2xl px-6 py-6">
-    <ContentDoc class="prose">
-      <template #not-found>
-        <p>404 not found</p>
-        <p>back to <nuxt-link to="/">index</nuxt-link>.</p>
-      </template>
-    </ContentDoc>
-  </main>
+  <ContentDoc class="prose">
+    <template #not-found>
+      <p>404 not found</p>
+      <p>back to <nuxt-link to="/">index</nuxt-link>.</p>
+    </template>
+  </ContentDoc>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,20 @@
 <template>
-  <div>index</div>
+  <div class="">
+    <section>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+      voluptatibus eius omnis hic maxime tempore itaque, ducimus repudiandae
+      quam eligendi cupiditate veritatis porro dolorem? Dicta quod deserunt
+      voluptas incidunt alias!
+    </section>
+    <section>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio assumenda
+      dignissimos impedit. Vel quae praesentium illum voluptatem nobis. Labore
+      beatae quos ipsam natus, eos placeat adipisci tempore dolorum fugiat
+      repudiandae nulla, quod aliquam ut quo enim. Odio architecto magni at
+      fugit quas officia rerum dolorum sapiente corrupti consectetur eveniet,
+      fuga, minus, aspernatur sequi deserunt nam et. Et cumque eveniet
+      asperiores sit quis nobis voluptatibus, ullam voluptates quaerat nam rem
+      excepturi ipsam quae laudantium sunt ex corporis. Sapiente a alias non!
+    </section>
+  </div>
 </template>
