34256c85b715f2cf3edc2a79014c874259cd54b8
Yuehu Lin <dev@fntsr.tw>
2023-03-28 01:32:51 +0800

    fix and refactor by chatgpt

--- server/plugins/wikilink.ts
+++ server/plugins/wikilink.ts
@@ -1,41 +1,30 @@
 export default defineNitroPlugin((nitroApp) => {
-  const imageRemark = {
-    with_width: {
-      regexp: /!\[\[(.+\.(jpg|png|jpeg))\|(\d+)\]\]/g,
-      markup: '![$1]($1){ style="width:$3px;" }',
-    },
-    with_width_height: {
-      regexp: /!\[\[(.+\.(jpg|png|jpeg))\|(\d+)x(\d+)\]\]/g,
-      markup: '![$1]($1){ style="width:$3px;height:$4px;" }',
-    },
-  }
-
-  const wikilinkRemark = {
-    normal: {
-      regexp: /\[\[(.+)\]\]/g,
-      markup: '[$1]($1)',
-    },
-    with_alias: {
-      regexp: /\[\[(.+)\|(.+)\]\]/g,
-      markup: '[$2]($1)',
-    },
-  }
-
-  const applyRemark = function (
-    text: string,
-    replacement: { regexp: RegExp; markup: string }
-  ) {
-    const { regexp, markup } = replacement
-    return text.replace(regexp, markup)
+  function replaceWikiLinksWithImages(input: string): string {
+    const pattern =
+      /(!)?\[\[([\w.]+)(?:\|([\w\s]+))?(?:(?:\|(\d+)(?:x(\d+))?)?)?\]\]/g
+    const output = input.replace(
+      pattern,
+      (_, isImageLink, filename, caption, width, height) => {
+        if (isImageLink) {
+          const captionText = caption || filename
+          const url = filename.replace(/\s/g, '_')
+          const imgTag = `<img src="${url}" alt="${captionText}"`
+          const style = ` style="width:${width ?? 'auto'}px; height:${
+            height ?? 'auto'
+          }px;"`
+          const closingTag = '/>'
+          return `${imgTag}${style}${closingTag}`
+        } else {
+          return `[${caption || filename}](${filename.replace(/\s/g, '_')})`
+        }
+      }
+    )
+    return output
   }
 
   nitroApp.hooks.hook('content:file:beforeParse', (file) => {
     if (file._id.endsWith('.md')) {
-      // apply_remark list is ordered
-      file.body = applyRemark(file.body, imageRemark.with_width)
-      file.body = applyRemark(file.body, imageRemark.with_width_height)
-      file.body = applyRemark(file.body, wikilinkRemark.with_alias)
-      file.body = applyRemark(file.body, wikilinkRemark.normal)
+      file.body = replaceWikiLinksWithImages(file.body)
     }
   })
 })
