21c8eaae3dcd080a6703b869c5d697935b42552e
Yuehu Lin <dev@fntsr.tw>
2023-04-24 03:48:05 +0800

    refactor(layout): extract layout to components

--- app.vue
+++ app.vue
@@ -1,5 +1,17 @@
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <div>
+    <LayoutWrapper>
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </LayoutWrapper>
+  </div>
 </template>
+<script setup>
+useHead({
+  bodyAttrs: {
+    class:
+      'bg-white text-black antialiased dark:bg-neutral-900 dark:text-white',
+  },
+})
+</script>
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,8 +1,15 @@
 <template>
-  <ContentDoc class="prose">
+  <ContentDoc class="prose dark:prose-invert">
     <template #not-found>
       <p>404 not found</p>
       <p>back to <nuxt-link to="/">index</nuxt-link>.</p>
     </template>
   </ContentDoc>
 </template>
+
+<style>
+.prose li {
+  margin-top: 0em;
+  margin-bottom: 0em;
+}
+</style>
