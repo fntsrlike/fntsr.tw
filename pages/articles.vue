09c65c5de4c0ff64b818f88309b99f856bc2887b
Yuehu Lin <dev@fntsr.tw>
2023-04-25 23:09:53 +0800

    refactor(search): extract search bar component

--- pages/articles.vue
+++ pages/articles.vue
@@ -4,32 +4,7 @@
       <PageTitle>
         {{ title }}
       </PageTitle>
-      <div class="relative max-w-lg">
-        <label>
-          <span class="sr-only">Search articles</span>
-          <input
-            v-model="searchValue"
-            aria-label="Search articles"
-            type="text"
-            placeholder="Search articles"
-            class="block w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100"
-          />
-        </label>
-        <svg
-          class="absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-gray-300"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="{2}"
-            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-          />
-        </svg>
-      </div>
+      <SearchBar v-model="searchValue" placeholder-text="Search articles" />
     </div>
     <ul>
       <li v-for="item in filteredBlogPosts" :key="item._path" class="py-4">
