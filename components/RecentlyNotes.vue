f9272bace962ee9e1b614ffef9897ffd096e4a78
Yuehu Lin <dev@fntsr.tw>
2023-04-27 00:42:42 +0800

    refactor(index):extract recently components

---
+++ components/RecentlyNotes.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <h2>近期更新的筆記：</h2>
+    <div>
+      <ul>
+        <li v-for="content in notes" :key="content._path">
+          <nuxt-link :to="content._path"> {{ content.title }}</nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script setup>
+const props = defineProps({
+  count: {
+    type: Number,
+    default: 5,
+  },
+})
+
+const notes = await useAsyncData(() =>
+  queryContent('notes').limit(props.count).find()
+).data
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -4,32 +4,7 @@
       <h2>Digital Garden</h2>
       Digital Garden 的一番介紹
     </section>
-    <section>
-      <h2>近期新增文章：</h2>
-      <ol>
-        <li v-for="content in articles" :key="content._path">
-          {{ DateTime.fromISO(content.created_at).toLocaleString() }}
-          <nuxt-link :to="content._path">{{ content.title }}</nuxt-link>
-        </li>
-      </ol>
-    </section>
-    <section>
-      <h2>近期更新的筆記：</h2>
-      <div>
-        <span v-for="content in notes" :key="content._path">
-          <nuxt-link :to="content._path">
-            {{ DateTime.fromISO(content.created_at).toLocaleString() }}
-            {{ content.title }}</nuxt-link
-          >、
-        </span>
-      </div>
-    </section>
+    <RecentlyArticles :count="5"></RecentlyArticles>
+    <RecentlyNotes :count="5"></RecentlyNotes>
   </div>
 </template>
-
-<script setup lang="ts">
-import { DateTime } from 'luxon'
-
-const notes = await useAsyncData(() => queryContent('notes').find()).data
-const articles = await useAsyncData(() => queryContent('articles').find()).data
-</script>
