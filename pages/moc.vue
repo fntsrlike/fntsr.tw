e1b55cfc114e0c3ef5823ff4710956d50f8f89a9
Yuehu Lin <dev@fntsr.tw>
2023-04-26 19:59:57 +0800

    feat(moc): add search bar

--- pages/moc.vue
+++ pages/moc.vue
@@ -1,19 +1,33 @@
-<script setup>
-const { data } = await useAsyncData(() => queryContent('moc').find())
-</script>
-
 <template>
-  <div class="container mx-auto">
-    <div class="text-center">
-      <h1 class="text-4xl">MoC</h1>
-    </div>
-    <div>There are {{ data.length }} available moc</div>
-    <div class="my-5">
-      <ul>
-        <li v-for="content in data" :key="content._path">
-          <nuxt-link :to="content._path">{{ content.title }}</nuxt-link>
-        </li>
-      </ul>
-    </div>
-  </div>
+  <ListLayout>
+    <ListHeader>
+      <PageTitle> MoC </PageTitle>
+      <SearchBar v-model="searchValue" placeholder-text="Search MoC" />
+    </ListHeader>
+    <ul>
+      <li v-for="item in filteredPosts" :key="item._path" class="py-4">
+        <PostItem :item="item" />
+      </li>
+    </ul>
+  </ListLayout>
 </template>
+<script setup>
+import { ref } from 'vue'
+import { DateTime } from 'luxon'
+
+const searchValue = ref('')
+
+const { data } = await useAsyncData(() => queryContent('moc').find())
+
+const filteredPosts = computed(() => {
+  return data.value.filter((post) => {
+    const searchContent =
+      post.title +
+      post.title_en +
+      post.description +
+      post.tags.map((tag) => tag.split(' ').join('-')) +
+      DateTime.fromISO(post.published_at).toFormat('yyyy-LL-dd')
+    return searchContent.toLowerCase().includes(searchValue.value.toLowerCase())
+  })
+})
+</script>
