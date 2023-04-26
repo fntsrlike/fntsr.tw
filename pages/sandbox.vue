0316f28d2a49a4ed1aef3960aa2ae500e50c0ef5
Yuehu Lin <dev@fntsr.tw>
2023-04-26 20:00:12 +0800

    feat(sandbox): add search bar

--- pages/sandbox.vue
+++ pages/sandbox.vue
@@ -1,21 +1,33 @@
-<script setup>
-const { data } = await useAsyncData(() => queryContent('sandbox').find())
-</script>
-
 <template>
-  <div class="container mx-auto">
-    <div class="text-center">
-      <h1 class="text-4xl">sandbox</h1>
-    </div>
-    <div>
-      <h1>There are {{ data.length }} available sandbox</h1>
-    </div>
-    <div class="my-5">
-      <ul v-for="content in data" :key="content._path">
-        <li>
-          <nuxt-link :to="content._path">{{ content.title }}</nuxt-link>
-        </li>
-      </ul>
-    </div>
-  </div>
+  <ListLayout>
+    <ListHeader>
+      <PageTitle> Sandbox </PageTitle>
+      <SearchBar v-model="searchValue" placeholder-text="Search sandbox" />
+    </ListHeader>
+    <ul>
+      <li v-for="item in filteredPosts" :key="item._path" class="py-4">
+        <PostItem :item="item" />
+      </li>
+    </ul>
+  </ListLayout>
 </template>
+<script setup>
+import { ref } from 'vue'
+import { DateTime } from 'luxon'
+
+const searchValue = ref('')
+
+const { data } = await useAsyncData(() => queryContent('sandbox').find())
+
+const filteredPosts = computed(() => {
+  return data.value.filter((post) => {
+    const searchContent =
+      post.title +
+      post.title_en +
+      post.description +
+      post.tags?.map((tag) => tag.split(' ').join('-')) +
+      DateTime.fromISO(post.published_at).toFormat('yyyy-LL-dd')
+    return searchContent.toLowerCase().includes(searchValue.value.toLowerCase())
+  })
+})
+</script>
