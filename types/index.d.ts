cbc805e1cf2e3a7c6b91fcf2055e6f281883614f
Yuehu Lin <dev@fntsr.tw>
2023-05-01 03:06:25 +0800

    feat(post): aadd post page

---
+++ types/index.d.ts
@@ -0,0 +1,14 @@
+
+interface Post {
+  title: string
+  title_en: string
+  slug: string
+  aliases: string[]
+  description: string
+  created_at: string
+  published_at: string
+  updated_at: string
+  tags: string[]
+}
+
+export {Post}
