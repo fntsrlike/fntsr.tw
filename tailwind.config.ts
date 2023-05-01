8cabdff708be1b91dd4a3d5e5b00971fd9d2308d
Yuehu Lin <dev@fntsr.tw>
2023-05-02 02:28:21 +0800

    refactor(prose): extract li style to config
    https://tailwindcss.com/docs/typography-plugin#customizing-the-css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,4 +4,19 @@ export default <Partial<Config>>{
   plugins: [require('@tailwindcss/typography')],
   darkMode: 'class',
   mode: 'jit',
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          // this is for prose class
+          css: {
+            li: {
+              'margin-top': '0em',
+              'margin-bottom': '0em',
+            },
+          },
+        },
+      },
+    },
+  },
 }
