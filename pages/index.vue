84cfa56bd65ee7f7754fad5590a35cdb3b33fabe
Yuehu Lin <dev@fntsr.tw>
2023-05-01 23:38:51 +0800

    refactor(vault): move index description to content

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="prose dark:prose-invert">
-    <section>
-      <h2>Digital Garden</h2>
-      Digital Garden 的一番介紹
-    </section>
+    <ContentDoc>
+      <template #default="{ doc }">
+        <PageTitle>{{ doc.title }}</PageTitle>
+        <ContentRenderer :value="doc" />
+      </template>
+      <template #not-found>
+        <p>There has no markdown file for index.</p>
+      </template>
+    </ContentDoc>
     <RecentlyArticles :count="5"></RecentlyArticles>
     <RecentlyNotes :count="5"></RecentlyNotes>
   </div>
