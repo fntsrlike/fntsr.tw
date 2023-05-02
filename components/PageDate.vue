1a8293e98e9a4299bb39cc17c009fc5945cb6da7
Yuehu Lin <dev@fntsr.tw>
2023-05-02 22:23:25 +0800

    refactor(page): extract page components

---
+++ components/PageDate.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="text-sm text-gray-600 dark:text-gray-400">
+    <time
+      v-if="publishedAt"
+      :datetime="publishedAt"
+      :title="DateTime.fromISO(publishedAt).toFormat(dateTimeFormat)"
+    >
+      發表於
+      {{ DateTime.fromISO(publishedAt).toFormat(dateFormat) }}
+    </time>
+    <time
+      v-if="updatedAt"
+      :datetime="updatedAt"
+      :title="DateTime.fromISO(updatedAt).toFormat(dateTimeFormat)"
+    >
+      ，並於
+      {{ DateTime.fromISO(updatedAt).toFormat(dateFormat) }}
+      更新
+    </time>
+  </div>
+</template>
+<script setup lang="ts">
+import { DateTime } from 'luxon'
+
+defineProps<{
+  publishedAt: string
+  updatedAt: string
+}>()
+
+const dateFormat = 'yyyy-LL-dd hh:mm'
+const dateTimeFormat = `${dateFormat} hh:mm`
+</script>
