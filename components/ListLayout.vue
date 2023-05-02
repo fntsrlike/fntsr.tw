596f0770dfda9370f1292107345b7d9ad0afae12
Yuehu Lin <dev@fntsr.tw>
2023-05-02 22:28:43 +0800

    refactor(page): extract post list page AND add page content

--- components/ListLayout.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="divide-y divide-gray-200 dark:divide-gray-700">
-    <slot />
-  </div>
-</template>
--- pages/notes/index.vue
+++ pages/notes/index.vue
@@ -1,33 +1,10 @@
 <template>
-  <ListLayout>
-    <ListHeader>
-      <PageTitle> Notes </PageTitle>
-      <SearchBar v-model="searchValue" placeholder-text="Search notes" />
-    </ListHeader>
-    <ul>
-      <li v-for="item in filteredPosts" :key="item._path" class="py-4">
-        <PostItem :item="item" />
-      </li>
-    </ul>
-  </ListLayout>
+  <ListPage :posts="data" />
 </template>
 <script setup>
-import { ref } from 'vue'
-import { DateTime } from 'luxon'
-
-const searchValue = ref('')
-
-const { data } = await useAsyncData(() => queryContent('notes').find())
-
-const filteredPosts = computed(() => {
-  return data.value.filter((post) => {
-    const searchContent =
-      post.title +
-      post.title_en +
-      post.description +
-      post.tags.map((tag) => tag.split(' ').join('-')) +
-      DateTime.fromISO(post.published_at).toFormat('yyyy-LL-dd')
-    return searchContent.toLowerCase().includes(searchValue.value.toLowerCase())
-  })
-})
+const { data } = await useAsyncData(() =>
+  queryContent('notes')
+    .where({ slug: { $ne: 'notes' } })
+    .find()
+)
 </script>
