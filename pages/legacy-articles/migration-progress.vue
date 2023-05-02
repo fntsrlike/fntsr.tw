869ab10f4ed3c667fdd6f20d43dc0bf59d2b9576
Yuehu Lin <dev@fntsr.tw>
2023-05-02 22:29:12 +0800

    migration progress

--- pages/legacy-articles/migration-progress.vue
+++ pages/legacy-articles/migration-progress.vue
@@ -1,42 +1,49 @@
 <template>
-  <article class="prose dark:prose-invert max-w-full">
-    <table>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th class="w-28">Published At</th>
-          <th>Title</th>
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="(article, index) in data"
-          :key="article.slug"
-          :class="
-            article.published_at ? '' : 'text-gray-400 dark:text-gray-500'
-          "
-        >
-          <td>{{ index + 1 }}</td>
-          <td>
-            {{ DateTime.fromISO(article.created_at).toFormat('yyyy-LL-dd') }}
-          </td>
-          <td>
-            <NuxtLink
-              v-if="article.published_at"
-              :href="`/${article.direct_to}/${DateTime.fromISO(
-                article.created_at
-              ).toFormat('yyyy-LL-dd')}-${article.slug}`"
-              >{{ article.title }}</NuxtLink
-            >
-            <span v-else>
-              {{ article.title }}
-            </span>
-          </td>
-          <td>{{ getStatus(article) }}</td>
-        </tr>
-      </tbody>
-    </table>
+  <article class="max-w-full">
+    <ContentDoc v-slot="{ doc }">
+      <PostHeader :post="doc"></PostHeader>
+      <PostContent :post="doc"></PostContent>
+    </ContentDoc>
+    <div class="prose dark:prose-invert max-w-full py-6">
+      <h2>Progress Table</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th class="w-28">Published At</th>
+            <th>Title</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="(article, index) in data"
+            :key="article.slug"
+            :class="
+              article.published_at ? '' : 'text-gray-400 dark:text-gray-500'
+            "
+          >
+            <td>{{ index + 1 }}</td>
+            <td>
+              {{ DateTime.fromISO(article.created_at).toFormat('yyyy-LL-dd') }}
+            </td>
+            <td>
+              <NuxtLink
+                v-if="article.published_at"
+                :href="`/${article.direct_to}/${DateTime.fromISO(
+                  article.created_at
+                ).toFormat('yyyy-LL-dd')}-${article.slug}`"
+                >{{ article.title }}</NuxtLink
+              >
+              <span v-else>
+                {{ article.title }}
+              </span>
+            </td>
+            <td>{{ getStatus(article) }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </article>
 </template>
 <script setup>
