cf1c467fa3b2e944a32b38cad2378c7df068c2c2
Yuehu Lin <dev@fntsr.tw>
2023-05-02 22:23:25 +0800

    refactor(page): extract page components

--- components/PageTitle.vue
+++ components/PageTitle.vue
@@ -4,8 +4,8 @@
   <h1 class="
       tracking-tight font-extrabold 
       text-gray-900 dark:text-gray-100 
-      text-3xl leading-9 mb-2
-      sm:text-4xl sm:leading-10 sm:mb-4
+      sm:text-3xl sm:leading-8 sm:mb-2
+         text-2xl    leading-6    mb-1
     "
   >
     <slot />
