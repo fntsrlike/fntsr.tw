7c5110b2e0b861a617acdee8282ba52ac793e7bd
Yuehu Lin <dev@fntsr.tw>
2023-05-03 03:06:20 +0800

    feat(layout): change width of section container

--- components/SectionContainer.vue
+++ components/SectionContainer.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">
+  <section class="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-4xl xl:px-0">
     <slot />
   </section>
 </template>
