b81ae48870b60a52194822de81d7d0fcb3a85963
Yuehu Lin <dev@fntsr.tw>
2023-05-02 22:23:25 +0800

    extract page components

---
+++ components/PageSubtitle.vue
@@ -0,0 +1,13 @@
+<!-- eslint-disable vue/first-attribute-linebreak -->
+<!-- eslint-disable prettier/prettier -->
+<template>
+  <h2 class="
+      tracking-tight font-medium 
+      text-gray-700 dark:text-gray-300 
+      sm:text-xl sm:leading-8 sm:mb-2
+         text-lg    leading-7    mb-2
+    "
+  >
+    <slot />
+  </h2>
+</template>
--- components/PostHeader.vue
+++ components/PostHeader.vue
@@ -1,42 +1,18 @@
 <template>
   <header class="mb-4">
-    <h1
-      class="text-gray-900 dark:text-gray-100 tracking-tight font-extrabold text-3xl sm:text-4xl leading-9 sm:leading-10 mb-2"
-    >
+    <PageTitle>
       {{ post.title }}
-    </h1>
-    <h2
-      v-if="post.title_en"
-      lang="en"
-      class="text-gray-700 dark:text-gray-300 tracking-tight font-extrabold text-xl sm:text-2xl leading-9 sm:leading-10 mb-2"
-    >
+    </PageTitle>
+    <PageSubtitle v-if="post.title_en" lang="en">
       {{ post.title_en }}
-    </h2>
-    <div class="text-sm text-gray-600 dark:text-gray-400">
-      <time
-        v-if="post.published_at"
-        :datetime="post.published_at"
-        :title="
-          DateTime.fromISO(post.published_at).toFormat('yyyy-LL-dd hh:mm')
-        "
-      >
-        發表於
-        {{ DateTime.fromISO(post.published_at).toFormat('yyyy-LL-dd') }}
-      </time>
-      <time
-        v-if="post.updated_at"
-        :datetime="post.updated_at"
-        :title="DateTime.fromISO(post.updated_at).toFormat('yyyy-LL-dd hh:mm')"
-      >
-        ，並於
-        {{ DateTime.fromISO(post.updated_at).toFormat('yyyy-LL-dd') }}
-        更新
-      </time>
-    </div>
+    </PageSubtitle>
+    <PageDate
+      :published-at="post.published_at"
+      :updated-at="post.published_at"
+    />
   </header>
 </template>
 <script setup lang="ts">
-import { DateTime } from 'luxon'
 import { Post } from '@/types/index'
 
 defineProps<{
