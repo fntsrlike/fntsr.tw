00eb2f216fbe393016b4f3bf0da494bf2cd6906d
Yuehu Lin <dev@fntsr.tw>
2023-06-11 01:19:48 +0800

    refactor(post): re-style post header

--- components/PostHeader.vue
+++ components/PostHeader.vue
@@ -6,10 +6,7 @@
     <PageSubtitle v-if="post.title_en" lang="en">
       {{ post.title_en }}
     </PageSubtitle>
-    <PageDate
-      :published-at="post.published_at"
-      :updated-at="post.published_at"
-    />
+    <PageDate :published-at="post.published_at" :updated-at="post.updated_at" />
   </header>
 </template>
 <script setup lang="ts">
