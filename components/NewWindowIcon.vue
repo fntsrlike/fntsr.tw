42b695354fae8306e1519040bf67a6cdb9b9ffda
Yuehu Lin <dev@fntsr.tw>
2023-06-20 16:06:55 +0800

    feat(prose): add new window icon for outgoing link

---
+++ components/NewWindowIcon.vue
@@ -0,0 +1,17 @@
+<template>
+  <Icon
+    name="fluent:window-new-24-regular"
+    class="fill-current text-gray-500 dark:text-gray-300"
+    :size="size / 4 + 'em'"
+    style="vertical-align: text-top"
+  />
+</template>
+
+<script setup>
+defineProps({
+  size: {
+    type: Number,
+    default: 3,
+  },
+})
+</script>
---
+++ components/content/ProseA.vue
@@ -0,0 +1,26 @@
+<template>
+  <NuxtLink :href="href" :target="theTarget">
+    <slot />
+    <NewWindowIcon v-if="isOutGoingLink" :size="3" class="ml-px" />
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  href: {
+    type: String,
+    default: '',
+  },
+  target: {
+    type: String,
+    default: undefined,
+    required: false,
+  },
+})
+
+const isOutGoingLink = computed(() => {
+  return props.href.startsWith('http')
+})
+
+const theTarget = props.target ?? (isOutGoingLink.value ? '_blank' : undefined)
+</script>
