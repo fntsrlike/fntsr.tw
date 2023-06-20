4b69ca6f0bb8c32d92259445599dfac2dea8a4b0
Yuehu Lin <dev@fntsr.tw>
2023-06-20 12:22:50 +0800

    refactor(recently): extract list

---
+++ components/RecentlyDateList.vue
@@ -0,0 +1,37 @@
+<template>
+  <section>
+    <h3>{{ title }}</h3>
+    <ol>
+      <li v-for="post in posts" :key="post._path">
+        {{ DateTime.fromISO(post.created_at).toLocal().toFormat('yyyy-LL-dd') }}
+        <NuxtLink :to="post._path">{{ post.title }}</NuxtLink>
+      </li>
+    </ol>
+  </section>
+</template>
+
+<script setup>
+import { DateTime } from 'luxon'
+import { useQuery } from '@/composables/useQuery'
+
+const { quertRecentlyPosts } = useQuery()
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: 'default title',
+  },
+  type: {
+    type: String,
+    default: '',
+  },
+  count: {
+    type: Number,
+    default: 5,
+  },
+})
+
+const posts = await quertRecentlyPosts(props.type, props.count)
+
+console.log(posts.value)
+</script>
---
+++ components/RecentlyNameList.vue
@@ -0,0 +1,40 @@
+<template>
+  <section>
+    <h3>{{ title }}</h3>
+    <div>
+      <ul class="list-none px-0">
+        <li
+          v-for="(post, index) in posts"
+          :key="post._path"
+          class="inline-block px-0"
+        >
+          <NuxtLink :to="post._path"> {{ post.title }}</NuxtLink
+          ><span v-if="index + 1 < posts.length">、</span>
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import { useQuery } from '@/composables/useQuery'
+
+const { quertRecentlyPosts } = useQuery()
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: 'default title',
+  },
+  type: {
+    type: String,
+    default: '',
+  },
+  count: {
+    type: Number,
+    default: 5,
+  },
+})
+
+const posts = await quertRecentlyPosts(props.type, props.count)
+</script>
