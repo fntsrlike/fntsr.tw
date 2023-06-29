7e0dbb6e8911b52c81bee1724f6961638b82b4c6
Yuehu Lin <dev@fntsr.tw>
2023-06-30 00:00:56 +0800

    feat(post): add TLDR callout

---
+++ components/content/Tldr.vue
@@ -0,0 +1,9 @@
+<template>
+  <div
+    class="bg-slate-100 border-slate-500 text-slate-700 dark:bg-slate-800 dark:border-slate-500 dark:text-slate-100 border-l-4 p-2"
+    role="alert"
+  >
+    <div class="font-bold my-1">TL;DR</div>
+    <div class="text-md my-1"><slot /></div>
+  </div>
+</template>
