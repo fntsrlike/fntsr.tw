6936e4234ee751163d540f6634cbdd016066630c
Yuehu Lin <dev@fntsr.tw>
2023-06-30 00:00:30 +0800

    feature(prose): decrease space between ul/ol
    # feat: new features.
    # fix: bug fixes.
    # docs: documentation changes.
    # refactor: refactor of a particular code section without introducing new features or bug fixes.
    # style: code style improvements.
    # perf: performance improvements.
    # test: changes to the test suite.
    # ci: changes to the CI system.
    # build: changes to the build system (we don't yet have one so this shouldn't apply).
    # chore: for other changes that don't match previous types. This doesn't appear in the changelog.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,13 @@
 // Learn more at https://tailwindcss.com/docs/configuration
 import type { Config } from 'tailwindcss'
+
+const round = (num: number) =>
+  num
+    .toFixed(7)
+    .replace(/(\.[0-9]+?)0+$/, '$1')
+    .replace(/\.0$/, '')
+const em = (px: number, base: number) => `${round(px / base)}em`
+
 export default <Partial<Config>>{
   plugins: [require('@tailwindcss/typography')],
   darkMode: 'class',
@@ -15,6 +23,10 @@ export default <Partial<Config>>{
             li: {
               'margin-top': '0em',
               'margin-bottom': '0em',
+            },
+            'ul ul, ul ol, ol ul, ol ol': {
+              marginTop: em(4, 16),
+              marginBottom: em(4, 16),
             },
           },
         },
