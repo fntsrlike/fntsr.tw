9ed0a536775a04f71b090a7d5b999cdb7596b3bb
Yuehu Lin <dev@fntsr.tw>
2023-07-31 22:22:57 +0800

    feat(post): move ToC to right side bar when md

--- components/PostContent.vue
+++ components/PostContent.vue
@@ -1,7 +1,14 @@
 <template>
-  <div class="my-4 break-word">
-    <TableOfContent :toc="post.body.toc.links"></TableOfContent>
-    <ContentRenderer :value="post" class="prose dark:prose-invert" />
+  <div class="my-4 break-word flex flex-col md:flex-row-reverse">
+    <div class="md:basis-1/4">
+      <TableOfContent
+        class="my-4 prose dark:prose-invert"
+        :toc="post.body.toc.links"
+      ></TableOfContent>
+    </div>
+    <div class="md:basis-3/4 md:mr-4">
+      <ContentRenderer :value="post" class="prose dark:prose-invert" />
+    </div>
   </div>
 </template>
 
