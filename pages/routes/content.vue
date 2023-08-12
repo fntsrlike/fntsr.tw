a953b626ee3f1e83d66194a0729468e40d0a5a9c
Yuehu Lin <dev@fntsr.tw>
2023-08-12 16:07:38 +0800

    feat(route): instead legacy style to tailwind

--- pages/routes/content.vue
+++ pages/routes/content.vue
@@ -4,15 +4,15 @@ const { data } = await useAsyncData('home', () => queryContent('/').find())
 
 <template>
   <div>
-    <div class="header">
+    <div>
       <h1>Routes overview</h1>
       <NuxtLink to="/">Go back to index</NuxtLink>
     </div>
-    <div class="info">
+    <div>
       <h1>There are {{ data.length }} available routes by nuxtjs</h1>
     </div>
-    <div class="wrapper">
-      <table id="routes">
+    <div>
+      <table class="table-auto border-collapse">
         <tr>
           <th class="text-lg text-center">content._id</th>
           <th class="text-lg text-center">content._path</th>
@@ -27,7 +27,7 @@ const { data } = await useAsyncData('home', () => queryContent('/').find())
         </tr>
       </table>
     </div>
-    <div class="info">
+    <div>
       The implementaion of this page is reference by
       <a
         href="https://wri7e.medium.com/view-all-routes-in-a-nuxtjs-application-193f38a715ac"
@@ -45,53 +45,3 @@ definePageMeta({
   layout: 'info',
 })
 </script>
-
-<style>
-.header {
-  padding: 30px;
-  background-color: rgba(5, 150, 105, 1);
-}
-.header h1 {
-  color: white;
-  font-size: 2rem;
-}
-.header a {
-  color: white;
-  font-size: 1rem;
-  text-decoration: underline;
-}
-.info {
-  padding: 30px;
-}
-.info h1 {
-  font-size: 1.2rem;
-}
-.info a {
-  color: blue;
-}
-.wrapper {
-  margin: 0 15px;
-}
-#routes {
-  border-collapse: collapse;
-  width: 100%;
-}
-#routes td,
-#routes th {
-  border: 1px solid #ddd;
-  padding: 8px;
-}
-#routes tr:nth-child(even) {
-  background-color: #f2f2f2;
-}
-#routes tr:hover {
-  background-color: #ddd;
-}
-#routes th {
-  background-color: rgba(5, 150, 105, 1);
-  padding-top: 12px;
-  padding-bottom: 12px;
-  text-align: left;
-  color: white;
-}
-</style>
