9c83388da6956aee6cb4dbee7b6d78366de03ee4
Yuehu Lin <dev@fntsr.tw>
2023-08-12 21:40:20 +0800

    refactor(route): extract list code to component

---
+++ components/RouteList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div>
+      <h1>There are {{ data.length }} available routes by nuxtjs</h1>
+    </div>
+    <div>
+      <table class="table-auto border-collapse">
+        <thead>
+          <tr>
+            <th class="text-lg text-center border">source</th>
+            <th class="text-lg text-center border">path</th>
+            <th class="text-lg text-center border">title</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="content in data" :key="content._id">
+            <td class="px-3 border">
+              {{ content._source }} ::
+              {{ content._path.split('/').slice(0, -1).join('/') }}
+            </td>
+            <td class="px-3 border">
+              <span>{{ content._path.split('/').pop() }}</span>
+            </td>
+            <td class="px-3 border">
+              <NuxtLink :to="content._path">{{ content.title }}</NuxtLink>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+<script setup>
+defineProps({
+  data: {
+    type: Array,
+    required: true,
+  },
+})
+</script>
--- pages/routes/content.vue
+++ pages/routes/content.vue
@@ -4,40 +4,7 @@
       <h1>Routes overview</h1>
       <NuxtLink to="/">Go back to index</NuxtLink>
     </div>
-    <div>
-      <h1>There are {{ data.length }} available routes by nuxtjs</h1>
-    </div>
-    <div>
-      <table class="table-auto border-collapse">
-        <tr>
-          <th class="text-lg text-center border">source</th>
-          <th class="text-lg text-center border">path</th>
-          <th class="text-lg text-center border">title</th>
-        </tr>
-        <tr v-for="content in data" :key="content._id">
-          <td class="px-3 border">
-            {{ content._source }} ::
-            {{ content._path.split('/').slice(0, -1).join('/') }}
-          </td>
-          <td class="px-3 border">
-            <span>{{ content._path.split('/').pop() }}</span>
-          </td>
-          <td class="px-3 border">
-            <NuxtLink :to="content._path">{{ content.title }}</NuxtLink>
-          </td>
-        </tr>
-      </table>
-    </div>
-    <div>
-      The implementaion of this page is reference by
-      <a
-        href="https://wri7e.medium.com/view-all-routes-in-a-nuxtjs-application-193f38a715ac"
-        target="_blank"
-      >
-        "42 - View all routes in a nuxtjs application"
-      </a>
-      .
-    </div>
+    <RouteList :data="data" />
   </div>
 </template>
 
