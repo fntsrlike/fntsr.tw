052828b558f9e338dbb32cc0025756ef22997f3f
Yuehu Lin <dev@fntsr.tw>
2023-08-15 13:40:35 +0800

    chore(ci): add prettier for tailwind.css

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,9 @@
 // prettier.config.js or .prettierrc.js
 module.exports = {
   // https://prettier.io/docs/en/options.html
+  plugins: [
+    'prettier-plugin-tailwindcss'
+  ],
   semi: false,
   singleQuote: true,
   printWidth: 100,
