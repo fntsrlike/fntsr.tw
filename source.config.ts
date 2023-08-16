046d99081455f6d23915df15749e433b8df0fb21
Yuehu Lin <dev@fntsr.tw>
2023-08-16 17:56:53 +0800

    feat(content): add more sources

--- source.config.ts
+++ source.config.ts
@@ -7,9 +7,23 @@ const genSource = (dirName = 'content', driver = 'fs') => {
   }
 }
 
-const gardenSrc = genSource('_write/content')
+const devSrc = genSource('_write/content-dev')
+const pagesSrc = genSource('_write/content')
+const gardenSrc = genSource('_write/content-garden')
+const legacySrc = genSource('_write/content-legacy')
 
 const sources: any = {}
-sources.garden = gardenSrc
+sources.garden = gardenSrc // basic content
+sources.pages = pagesSrc // page's content
+
+// To experience
+if (process.env.NODE_ENV === 'development') {
+  sources.zDev = devSrc
+}
+
+// Increase dev performance to avoid load lots of content
+if (process.env.NODE_ENV === 'production') {
+  sources.legacies = legacySrc
+}
 
 export default sources
