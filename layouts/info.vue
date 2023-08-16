167302b121c7bea2aa05716879efe3ebdced00c7
Yuehu Lin <dev@fntsr.tw>
2023-08-16 17:10:03 +0800

    feat(layout): add info layout

---
+++ layouts/info.vue
@@ -0,0 +1,9 @@
+<template>
+  <SectionContainer class="xl:text-lg">
+    <div class="flex h-screen flex-col justify-between font-sans">
+      <main class="mb-auto">
+        <slot />
+      </main>
+    </div>
+  </SectionContainer>
+</template>
