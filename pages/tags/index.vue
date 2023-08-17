84cfe0ae940a8ed1589f5b92df51ff411545495f
Yuehu Lin <dev@fntsr.tw>
2023-08-18 01:00:11 +0800

    feat(page): add tag page description

--- pages/tags/index.vue
+++ pages/tags/index.vue
@@ -1,28 +1,37 @@
 <template>
-  <div
-    class="flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0"
-  >
-    <div class="space-x-2 pt-6 pb-8 md:space-y-5">
-      <h1
-        class="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:border-r-2 md:px-6 md:text-6xl md:leading-14"
-      >
-        Tags
-      </h1>
-    </div>
-    <div class="flex max-w-lg flex-wrap">
-      <div v-for="tag in articleTags" :key="tag" class="mt-2 mb-2 mr-5">
-        <PostTag :text="tag" />
-        <NuxtLink
-          :href="'/tags/' + kebabCase(tag)"
-          class="-ml-2 text-sm font-semibold uppercase text-gray-600 dark:text-gray-300"
-          aria-label="
+  <div>
+    <div
+      class="flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0"
+    >
+      <div class="space-x-2 pt-6 pb-8 md:space-y-5">
+        <h1
+          class="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:border-r-2 md:px-6 md:text-6xl md:leading-14"
+        >
+          Tags
+        </h1>
+      </div>
+      <div class="flex max-w-lg flex-wrap">
+        <div v-for="tag in articleTags" :key="tag" class="mt-2 mb-2 mr-5">
+          <PostTag :text="tag" />
+          <NuxtLink
+            :href="'/tags/' + kebabCase(tag)"
+            class="-ml-2 text-sm font-semibold uppercase text-gray-600 dark:text-gray-300"
+            aria-label="
           posts
           tagged
           ${tag}`"
-        >
-          ({{ tagsCount[tag] }})
-        </NuxtLink>
+          >
+            ({{ tagsCount[tag] }})
+          </NuxtLink>
+        </div>
       </div>
+    </div>
+    <div
+      class="flex flex-col items-start justify-start mt-10 md:mt-16 md:flex-row md:items-center md:justify-center md:space-x-6"
+    >
+      <ContentDoc v-slot="{ doc }">
+        <ContentRenderer :value="doc" class="prose dark:prose-invert" />
+      </ContentDoc>
     </div>
   </div>
 </template>
