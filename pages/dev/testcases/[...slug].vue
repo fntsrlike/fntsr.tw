93d696755e9b6084c3ef02214da07105227e1cfa
Yuehu Lin <dev@fntsr.tw>
2023-08-22 13:59:24 +0800

    feat(dev): add test cases path files

---
+++ pages/dev/testcases/[...slug].vue
@@ -0,0 +1,11 @@
+<template>
+  <ContentDoc>
+    <template #default="{ doc }">
+      <PostPage :post="doc" />
+    </template>
+    <template #not-found>
+      <p>404: test cases not found</p>
+      <p>back to <NuxtLink to="/">index</NuxtLink>.</p>
+    </template>
+  </ContentDoc>
+</template>
