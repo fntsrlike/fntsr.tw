4259c24b852c6bc83d75c44655c252e81549eb4f
Yuehu Lin <dev@fntsr.tw>
2023-10-05 01:56:46 +0800

    refactor(component): extract list section

--- components/list/Page.vue
+++ components/list/Page.vue
@@ -1,65 +1,15 @@
 <template>
   <section class="pb-24">
-    <div class="divide-y divide-gray-200 dark:divide-gray-700">
-      <header class="pb-8 space-y-2 md:space-y-5">
-        <div>
-          <PageTitle>
-            {{ page.title }}
-          </PageTitle>
-          <PageSubtitle v-if="page.title_en" lang="en">
-            {{ page.title_en }}
-          </PageSubtitle>
-        </div>
-        <PostContent :post="page" />
-        <SearchBar v-model="searchValue" placeholder-text="Search announce" />
-      </header>
-      <main>
-        <ul>
-          <li v-for="item in filteredPosts" :key="item._path" class="py-4">
-            <ListItem :item="item" />
-          </li>
-        </ul>
-      </main>
-    </div>
+    <PageHeader :post="page" />
+    <PostContent :post="page"/>
+    <ListSection :posts="posts"/>
   </section>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue'
-import { DateTime } from 'luxon'
 import { Post } from '@/types/index'
-import { usePost } from '@/composables/usePost'
 
-const props = defineProps<{
+defineProps<{
   page: Post
   posts: Post[]
 }>()
-
-const { isDraft } = usePost()
-const searchValue = ref('')
-
-const list = props.posts.map((post) => {
-  const hasPostTags = !!post.tags
-  if (!hasPostTags) {
-    post.tags = []
-  }
-  return post
-})
-
-const filteredPosts = computed(() => {
-  return list.filter((post: Post) => {
-    const isProduct = !process.dev
-    if (isProduct && isDraft(post)) {
-      return false
-    }
-
-    const tags = post.tags.filter((tag) => !!tag)
-    const searchContent =
-      post.title +
-      post.title_en +
-      post.description +
-      tags.map((tag: string) => (tag ? tag.split(' ').join('-') : '')) +
-      DateTime.fromISO(post.published_at).toFormat('yyyy-LL-dd')
-    return searchContent.toLowerCase().includes(searchValue.value.toLowerCase())
-  })
-})
 </script>
---
+++ components/list/Section.vue
@@ -0,0 +1,49 @@
+<template>
+  <main class="divide-y divide-gray-200 dark:divide-gray-700">
+    <SearchBar v-model="searchValue" placeholder-text="Search announce" class="pb-8"/>
+    <ul>
+      <li v-for="item in filteredPosts" :key="item._path" class="py-4">
+        <ListItem :item="item" />
+      </li>
+    </ul>
+  </main>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { DateTime } from 'luxon'
+import { Post } from '@/types/index'
+import { usePost } from '@/composables/usePost'
+
+const props = defineProps<{
+  posts: Post[]
+}>()
+
+const { isDraft } = usePost()
+const searchValue = ref('')
+
+const list = props.posts.map((post) => {
+  const hasPostTags = !!post.tags
+  if (!hasPostTags) {
+    post.tags = []
+  }
+  return post
+})
+
+const filteredPosts = computed(() => {
+  return list.filter((post: Post) => {
+    const isProduct = !process.dev
+    if (isProduct && isDraft(post)) {
+      return false
+    }
+
+    const tags = post.tags.filter((tag) => !!tag)
+    const searchContent =
+      post.title +
+      post.title_en +
+      post.description +
+      tags.map((tag: string) => (tag ? tag.split(' ').join('-') : '')) +
+      DateTime.fromISO(post.published_at).toFormat('yyyy-LL-dd')
+    return searchContent.toLowerCase().includes(searchValue.value.toLowerCase())
+  })
+})
+</script>
