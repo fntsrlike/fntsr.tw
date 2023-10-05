9410d9559990f4cee3ee98bb4cf6fd1245b41449
Yuehu Lin <dev@fntsr.tw>
2023-10-05 14:24:06 +0800

    refactor(component): move ToC to Post

--- components/post/Content.vue
+++ components/post/Content.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="my-4 break-word flex flex-col md:flex-row-reverse">
     <div class="md:basis-1/4">
-      <TableOfContent
+      <PostTableOfContent
         class="my-4 prose dark:prose-invert"
         :toc="post.body.toc.links"
-      ></TableOfContent>
+      ></PostTableOfContent>
     </div>
     <div class="md:basis-3/4 md:mr-4">
       <ContentRenderer :value="post" class="prose dark:prose-invert" />
