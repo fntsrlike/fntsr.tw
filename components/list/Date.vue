7c2dcef60482797d7ef80a5edb7c976eff7922d5
Yuehu Lin <dev@fntsr.tw>
2023-10-05 14:39:30 +0800

    refactor(component): extract datatime to props

--- components/list/Date.vue
+++ components/list/Date.vue
@@ -1,30 +1,21 @@
 <template>
-  <dl>
+  <dl v-if="isoDateTime" >
     <dt class="sr-only">Published on</dt>
     <dd
       class="text-base font-medium leading-6 text-gray-500 dark:text-gray-400"
     >
-      <time v-if="item.published_at" dateTime="{{item.published_at}}">
-        {{
-          DateTime.fromISO(item.published_at).toLocal().toFormat('yyyy-LL-dd')
-        }}
-      </time>
-      <time
-        v-if="!item.published_at && item.created_at"
-        dateTime="{{item.created_at}}"
-      >
-        {{
-          DateTime.fromISO(item.created_at).toLocal().toFormat('yyyy-LL-dd')
-        }}
+      <time dateTime="{{isoDateTime}}" :title="isoToDateTime(isoDateTime)">
+        {{ isoToDate(isoDateTime) }}
       </time>
     </dd>
   </dl>
 </template>
 <script setup lang="ts">
-import { DateTime } from 'luxon'
-import { Post } from '@/types/index'
+import { useDateTime } from '@/composables/useDateTime'
+
+const { isoToDateTime, isoToDate } = useDateTime()
 
 defineProps<{
-  item: Post
+  isoDateTime: string | null
 }>()
 </script>
