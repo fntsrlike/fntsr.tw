314061f8982af2dfeb338733c4b74ec2244e99b3
Yuehu Lin <dev@fntsr.tw>
2023-10-05 14:23:49 +0800

    refactor(component): extract date component from list

---
+++ components/list/Date.vue
@@ -0,0 +1,30 @@
+<template>
+  <dl>
+    <dt class="sr-only">Published on</dt>
+    <dd
+      class="text-base font-medium leading-6 text-gray-500 dark:text-gray-400"
+    >
+      <time v-if="item.published_at" dateTime="{{item.published_at}}">
+        {{
+          DateTime.fromISO(item.published_at).toLocal().toFormat('yyyy-LL-dd')
+        }}
+      </time>
+      <time
+        v-if="!item.published_at && item.created_at"
+        dateTime="{{item.created_at}}"
+      >
+        {{
+          DateTime.fromISO(item.created_at).toLocal().toFormat('yyyy-LL-dd')
+        }}
+      </time>
+    </dd>
+  </dl>
+</template>
+<script setup lang="ts">
+import { DateTime } from 'luxon'
+import { Post } from '@/types/index'
+
+defineProps<{
+  item: Post
+}>()
+</script>
