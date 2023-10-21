901e921a601af1f6c89cdc42577b7f311fc39564
Yuehu Lin <dev@fntsr.tw>
2023-10-22 01:36:49 +0800

    feat(util): install nuxt-icon module...

--- app.config.ts
+++ app.config.ts
@@ -1,1 +1,14 @@
-export default defineAppConfig({})
+// app.config.ts
+export interface NuxtIconModuleOptions {
+  size?: string | false
+  class?: string
+  aliases?: { [alias: string]: string }
+}
+
+export default defineAppConfig({
+  nuxtIcon: {
+    size: '24px', // default <Icon> size applied
+    class: 'icon', // default <Icon> class applied
+    aliases: {},
+  } as NuxtIconModuleOptions,
+})
