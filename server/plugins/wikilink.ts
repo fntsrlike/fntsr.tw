6bf61f6b08f3978b06b0863aba371c2589b2d712
Yuehu Lin <dev@fntsr.tw>
2023-10-22 02:47:12 +0800

    feat(wikilink): improve wikiilink to pass testcases

--- server/plugins/wikilink.ts
+++ server/plugins/wikilink.ts
@@ -1,30 +1,57 @@
 export default defineNitroPlugin((nitroApp) => {
-  function replaceWikiLinksWithImages(input: string): string {
-    const pattern =
-      /(!)?\[\[([\w/._-]+)(?:\|([\w\s]+))?(?:(?:\|(\d+)(?:x(\d+))?)?)?\]\]/g
-    const output = input.replace(
-      pattern,
-      (_, isImageLink, filename, caption, width, height) => {
-        if (isImageLink) {
-          const captionText = caption || filename
-          const url = filename.replace(/\s/g, '_')
-          const imgTag = `<img src="/${url}" alt="${captionText}"`
-          const style = ` style="width:${
-            width ? width + 'px' : 'auto'
-          }; height:${height ? height + 'px' : 'auto'};"`
-          const closingTag = '/>'
-          return `${imgTag}${style}${closingTag}`
-        } else {
-          return `[${caption || filename}](${filename.replace(/\s/g, '_')})`
-        }
+  function convertWikiLink(text: string): string {
+    const renderRegExp = /!\[\[([\w/.]+)\|?([^[\]]+)?\]\]/g
+    const linkRegExp = /\[\[([\w/.]+)\|?([^[\]]+)?\]\]/g
+    const imageSizeRegExp = /!\[\[[\w/.]+\|(\d+)(?:[xX](\d+))?\]\]/
+
+    let isInCodeBlock = false
+    const convertedLines = text.split('\n').map((line) => {
+      const isCodeBlockSyntax = line.startsWith('```')
+      isInCodeBlock = isCodeBlockSyntax ? !isInCodeBlock : isInCodeBlock
+
+      if (!isInCodeBlock) {
+        line = convertImageMarkdown(line, imageSizeRegExp, renderRegExp)
+        line = convertLinkMarkdown(line, linkRegExp)
       }
-    )
-    return output
+      return line
+    })
+
+    return convertedLines.join('\n')
+  }
+
+  function convertImageMarkdown(
+    line: string,
+    imageSizeRegExp: RegExp,
+    renderRegExp: RegExp
+  ) {
+    return line.replace(renderRegExp, (match, imgPath, imgAlias) => {
+      let style = ''
+      const imgSizeMatch = imageSizeRegExp.exec(match)
+
+      if (imgSizeMatch !== null) {
+        const [, width, height] = imgSizeMatch
+        const styleWidth = width ? `width=${width}px` : ''
+        const styleHeight = height ? `height=${height}px` : ''
+        style = `{ ${styleWidth} ${styleHeight} }`
+      }
+
+      const filename = imgPath.split('/').pop()
+      const alias = imgSizeMatch || !imgAlias ? filename : imgAlias
+      const imgMarkdown = `![${alias}](${imgPath})${style}`
+      return imgMarkdown
+    })
+  }
+
+  function convertLinkMarkdown(line: string, linkRegExp: RegExp) {
+    return line.replace(linkRegExp, (_, linkPath, linkAlias) => {
+      const linkMarkdown = `[${linkAlias || linkPath}](${linkPath})`
+      return linkMarkdown
+    })
   }
 
   nitroApp.hooks.hook('content:file:beforeParse', (file) => {
     if (file._id.endsWith('.md')) {
-      file.body = replaceWikiLinksWithImages(file.body)
+      file.body = convertWikiLink(file.body)
     }
   })
 })
